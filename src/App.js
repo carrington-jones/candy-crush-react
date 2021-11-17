9d83ce83e83f85f21f4bff1841355d88a2c166de
Carrington Jones <carrington.jones47@gmail.com>
2021-11-16 23:21:39 -0600

    Creating game board

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import {useEffect, useState} from "react";
+
+const width = 8
+const candyColors = [
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'red',
+    'yellow'
+]
 
 
-function App() {
+
+const App= () => {
+
+    const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+    const createBoard = () => {
+        const randomColorArrangement =[]
+        for (let i = 0; i < width * width; i++){
+            const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)] //Grabbing random color from candyColors array
+            randomColorArrangement.push(randomColor)
+        }
+        setCurrentColorArrangement(randomColorArrangement)
+    }
+
+    useEffect(() => {
+        createBoard()
+    },[])
+
+    console.log(currentColorArrangement);
+
   return (
       <div>
 
